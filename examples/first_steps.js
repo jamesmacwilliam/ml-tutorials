166171f93f5f0c81ae899382cc44934d6f0d0d98
James Mac William <jimmy.macwilliam@gmail.com>
2019-01-02 22:49:34 -0500

    add commented code

--- examples/first_steps.js
+++ examples/first_steps.js
@@ -3,4 +3,12 @@ import tf from '@tensorflow/tfjs'
 let x = () =>
   console.log('foo')
 
+//Set up a linear classifier.
+//let classifier = tf.estimator.LinearClassifier(feature_columns)
+
+//Train the model on some example data.
+//classifier.train(input_fn=train_input_fn, steps=2000)
+
+//Use it to predict.
+//let predictions = classifier.predict(input_fn=predict_input_fn)
 x()
