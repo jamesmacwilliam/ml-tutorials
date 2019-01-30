d111241eaf5cce9e13d8c73b4c58549b37a94749
James Mac William <jimmy.macwilliam@gmail.com>
2019-01-30 14:35:55 -0500

    capture script + docs

--- keras_find_me/README.md
+++ keras_find_me/README.md
@@ -9,6 +9,12 @@
 - install packages `pipenv install`
 - setup env `pipenv shell`
 
-
+## Training
+- with camera pointed away from subject
+  * `python capture_images.py data/valid/0 200`
+  * `python capture_images.py data/train/0 2000`
+- with camera pointed at subject
+  * `python capture_images.py data/valid/1 200`
+  * `python capture_images.py data/train/1 2000`
 
 * tutorial: https://medium.com/@jinilcs/a-simple-keras-model-on-my-laptop-webcam-dda77521e6a0
--- keras_find_me/capture_images.py
+++ keras_find_me/capture_images.py
@@ -10,3 +10,23 @@ imagecount = int(sys.argv[2])
 os.makedirs(directory, exist_ok=True)
 
 video = cv2.VideoCapture(0)
+
+filename = len(os.listdir(directory))
+count = 0
+
+while True and count < imagecount:
+    filename += 1
+    count += 1
+    _read_flag, frame = video.read()
+    im = Image.fromarray(frame, 'RGB')
+    im = im.resize((128,128))
+    im.save(os.path.join(directory, str(filename) + ".jpg"), "JPEG") # 0.jpg...etc
+
+    cv2.imshow("Capturing", frame) # display webcam with `capturing` header
+    key = cv2.waitKey(1)
+
+    if key == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
